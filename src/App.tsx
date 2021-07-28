329f715c4618d233edffb1bed0e3891d94170ba0
Codespaces <codespaces@github.com>
2021-07-28 09:57:38 +0000

    Codespaces

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from 'react';
 import { PointLightHelper } from 'three';
 import './App.css';
 import GizmoHelperComp from './common/helpers/GizmoHelperComp';
+import { ImageToParticlesShaders } from './lessons/image-to-particles-shaders';
 import PointsGeometryExample from './lessons/points-geometry-materials';
 import ShaderAnimation from './lessons/shader-example-1';
 
@@ -35,10 +36,10 @@ function App() {
   return (
     // position: [0, 0, 2750], fov: 100, near: 1, far: 1000
     <Canvas
-      camera={{ fov: 15, near: 5, far: 2000, aspect: window.innerWidth / window.innerHeight, position: [0, 10, 10] }}
+      camera={{ fov: 50, near: 1, far: 50000, aspect: window.innerWidth / window.innerHeight, position: [0, 0, 300] }}
     >
       <CameraComponent />
-      {/* <gridHelper /> */}
+      <gridHelper />
       <GizmoHelperComp />
       {/* <BufferGeometryShaders />
       <BufferGeometryShadersInteraction rotation={[-Math.PI/2, 0, 0]}/> 
@@ -46,10 +47,14 @@ function App() {
       <PointsGeometryExample/> */}
 
       {/* For Displacement shader */}
-      <ambientLight />
+      {/* <ambientLight />
       <color attach="background" args={["#00000F"]} />
       <ShaderAnimation rotation={[-Math.PI / 2, 0, 0]} />
-      <PointLightHelperComponent />
+      <PointLightHelperComponent /> */}
+
+      {/* Image to particles */}
+      <color attach="background" args={["#d3ffd6"]} />
+      <ImageToParticlesShaders />
 
     </Canvas>
   );
---
+++ src/lessons/image-to-particles-shaders.tsx
@@ -0,0 +1,162 @@
+import { useEffect, useRef, useState } from "react";
+import { DoubleSide, InstancedBufferAttribute, Texture, TextureLoader, Uint16BufferAttribute, Vector2 } from "three";
+
+export function ImageToParticlesTemplate(props: any) {
+
+    const ref = useRef();
+    const [positions, setPositions] = useState<Float32Array>(new Float32Array([
+        -0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0
+    ]));
+    const colors = new Float32Array([
+        //R, G, B, A
+        1, 0, 1, 1.0,
+        1, 1, 1, 1.0,
+        1, 1, 0, 1.0,
+        1, 0, 1, 1.0
+    ]);
+    const indices = new Uint16BufferAttribute([
+        0, 2, 1,
+        2, 1, 3
+    ], 1);
+
+    const vertexShaders = `
+        precision highp float;
+
+        uniform mat4 modelViewMatrix;
+        uniform mat4 projectionMatrix;
+        uniform vec2 uTextureSize;
+
+        attribute vec3 position;
+        attribute vec3 offset;
+        attribute vec4 color;
+
+        varying vec2 vPUv;
+
+        uniform sampler2D uTexture;
+        
+        void main() {
+        
+            vec2 puv = offset.xy / uTextureSize;
+            vPUv = puv;
+
+            // REF: https://stackoverflow.com/questions/13901119/how-does-vectors-multiply-act-in-shader-language
+
+            vec3 displaced = offset;
+            displaced.xy -= uTextureSize * 0.5;
+
+            vec4 mvPosition = modelViewMatrix * vec4(displaced, 1);
+            mvPosition.xyz += position;
+            gl_Position = projectionMatrix * mvPosition;
+        }
+    `;
+
+    const fragmentShaders = `
+        precision highp float;
+
+        uniform sampler2D uTexture;
+        
+        varying vec2 vPUv;
+
+        void main() {
+            gl_FragColor = texture2D(uTexture, vPUv);
+        }
+    `;
+
+
+    return (
+        <mesh>
+            <instancedBufferGeometry
+                attach="geometry"
+                index={indices}
+            >
+                <bufferAttribute
+                    attachObject={["attributes", "position"]}
+                    array={positions}
+                    itemSize={3}
+                    count={positions.length / 3}
+                />
+
+                <bufferAttribute
+                    attachObject={["attributes", "color"]}
+                    array={colors}
+                    itemSize={4}
+                    normalized={true}
+                    count={colors.length / 4}
+                />
+
+                <instancedBufferAttribute
+                    attachObject={["attributes", "offset"]}
+                    array={props.offset}
+                    itemSize={3}
+                    count={props.offset.length / 3}
+                />
+
+            </instancedBufferGeometry>
+            <rawShaderMaterial
+                attach="material"
+                uniforms={props.uniforms}
+                vertexShader={vertexShaders}
+                fragmentShader={fragmentShaders}
+                side={DoubleSide}
+                transparent={true}
+            />
+        </mesh>
+    )
+}
+
+export function ImageToParticlesShaders(props: any) {
+
+    const [offset, setOffset] = useState<Float32Array>();
+    const [uniforms, setUniforms] = useState<any>({});
+
+    useEffect(() => {
+        const myImgUrl = '/picture1.png';
+        (new TextureLoader()).load(myImgUrl, (texture: Texture) => {
+            if (!texture) {
+                return;
+            }
+
+            const threshold = 34;
+            const tempCanvEl = document.createElement('canvas');
+            const ctx = tempCanvEl.getContext('2d');
+            const textureImage = texture.image;
+            const totalPixels = textureImage.width * textureImage.height;
+
+            tempCanvEl.width = textureImage.width;
+            tempCanvEl.height = textureImage.height;
+
+            if (ctx) {
+                let totalVisiblePixels = 0;
+
+                ctx?.scale(1, 1);
+                ctx?.drawImage(textureImage, 0, 0, textureImage.width, textureImage.height);
+
+                const imgData = ctx.getImageData(0, 0, tempCanvEl.width, tempCanvEl.height);
+
+                const offsets = new Float32Array(totalPixels * 3);
+                const offsetsIndices = [];
+                for (let i = 0; i < totalPixels; i++) {
+                    offsets[i * 3 + 0] = i % textureImage.width;
+                    offsets[i * 3 + 1] = Math.floor(i / textureImage.width);
+
+                    offsetsIndices.push(i);
+                }
+
+                setOffset(offsets);
+                setUniforms({
+                    uTexture: texture,
+                    uTextureSize: { value: new Vector2(textureImage.width, textureImage.height) }
+                });
+            }
+        })
+    }, []);
+
+    return (
+        <>
+            {offset && <ImageToParticlesTemplate offset={offset} uniforms={uniforms} />}
+        </>
+    )
+}
